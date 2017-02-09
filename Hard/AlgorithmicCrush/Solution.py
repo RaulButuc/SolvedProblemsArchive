498c09f7a597b39504683e2a71771152619d9145
Raul Butuc <RaulButuc@users.noreply.github.com>
2017-02-09 12:54:19 +0000

    Added AlgorithmicCrush

---
+++ Hard/AlgorithmicCrush/Solution.py
@@ -0,0 +1,20 @@
+n, inputs = [int(n) for n in input().split(" ")]
+
+list = [0] * (n + 1)
+
+for _ in range(inputs):
+    x, y, incr = [int(n) for n in input().split(" ")]
+list[x - 1] += incr
+
+if (y <= len(list)):
+    list[y] -= incr;
+
+max = x = 0
+
+for i in list:
+    x = x + i;
+
+if (max < x):
+    max = x;
+
+print(max)
